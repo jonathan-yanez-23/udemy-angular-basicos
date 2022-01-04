60c514604fbcc9424e25923435d2594546d36a62
jonathan-yanez-23 <jiyanez@minsait.com>
2022-01-04 19:15:45 +0100

    First commit

---
+++ src/app/contador/contador/contador.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core"
+import { ContadorComponent } from "./contador.component";
+
+@NgModule({
+    declarations: [ContadorComponent],
+    exports: [ContadorComponent]
+})
+export class ContadorModule{}
---
+++ src/app/heroes/heroe/heroe.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+    selector: 'app-heroe',
+    templateUrl: 'heroe.component.html'
+})
+export class HeroeComponent{
+    nombre: string = 'IronMan';
+    edad: number = 43;
+    obtenerNombre(): string {
+        return `${this.nombre} - ${this.edad}`;
+    }
+    cambiarNombre(): void{
+        this.nombre = 'Spiderman';
+    }
+    cambiarEdad(): void{
+        this.edad = 30;
+    }
+    get nombreCapitalizado() {
+        console.log('hey')
+        return this.nombre.toUpperCase();
+    }
+}
